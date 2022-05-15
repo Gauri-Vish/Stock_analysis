fcb739994f31510542adb821ce18c7e1de0cb559
Gauri Vishwasrao <75183205+Gauri-Vish@users.noreply.github.com>
2022-05-15 22:40:07 +0530

    Update st_stock.py

--- st_stock.py
+++ st_stock.py
@@ -31,9 +31,9 @@ covid_ind_1='2020-01-27'
 covid_ind_1_end='2020-05-31'
 '''
 #age = st.sidebar.slider('Choose the period', 1, 5, 3)
-
-names={"Oil":["ONGC.NS","DE"],
-       "Agriculture":["GODREJAGRO.NS","XOM"],
+#Changed agro companies
+names={"Oil":["ONGC.NS","XOM"],
+       "Agriculture":["RALLIS.NS","BAYERCROP.NS"],
        "Tech":["TECHM.NS","IBM"],
        "Banking":["^NSEBANK","^BKX"]}
 
@@ -49,24 +49,32 @@ data_us = yf.download(tickers=selected_stock[1],
 data=pd.merge(data_ind["Close"], data_us["Close"], left_index=True, right_index=True)
 data.columns=["Inidan compnay","US compnay"]
 
-#visualization 
+#visualization #labelling #company info
 if layout=='Seprate':
         col1, col2 = st.columns(2)
         with col1:
-                st.markdown(f"<h5 style='text-align: center;'> {selected_stock[0]} </h5>",unsafe_allow_html=True) #labelling
+                st.markdown(f"<h5 style='text-align: center;'> {selected_stock[0]} </h5>",unsafe_allow_html=True) 
                 st.line_chart(data_ind.Close,width =500 ,height=250)
-                st.write('The Oil and Natural Gas Corporation is an Indian oil and gas explorer and producer. It is under the ownership of Ministry of Petroleum and Natural Gas and Government of India.')
+                if selected_stock[0]=='ONGC.NS':
+                    st.write('The Oil and Natural Gas Corporation is an Indian oil and gas explorer and producer. It is under the ownership of Ministry of Petroleum and Natural Gas and Government of India.')
+                elif selected_stock[0]=='RALLIS.NS':
+                    st.write("Rallis, a Tata Enterprise, is a subsidiary of Tata Chemicals, with its business presence in the farm essentials vertical. It is one of India's leading crop care companies.")
+                elif selected_stock[0]=='TECHM.NS':
+                    st.write('Tech Mahindra is an Indian multinational information technology services and consulting company. Part of the Mahindra Group, the company is headquartered in Pune')
+                elif selected_stock[0]=='^NSEBANK':
+                    st.write('')
         with col2:
-                st.markdown(f"<h5 style='text-align: center;'>{selected_stock[1]}</h5>",unsafe_allow_html=True) #labelling
+                st.markdown(f"<h5 style='text-align: center;'>{selected_stock[1]}</h5>",unsafe_allow_html=True)
                 st.line_chart(data_us.Close,width =500 ,height=250,)
-        from sklearn.preprocessing import MinMaxScaler ,StandardScaler
-        mms=MinMaxScaler()
-        data=pd.DataFrame(mms.fit_transform(data),columns=["Inidan compnay","US compnay"],index=data.index)
-        st.markdown(f"<h5 style='text-align: center;'>{selected_stock[0]} v/s {selected_stock[1]}</h5>",unsafe_allow_html=True) #labelling 
-        st.line_chart(data,)
-
-
-
+                if selected_stock[1]=='XOM':
+                    st.write('Exxon Mobil Corporation, stylized as ExxonMobil, is an American multinational oil and gas corporation headquartered in Irving, Texas.')
+                elif selected_stock[1]=='BAYERCROP.NS':
+                    st.write("Crop Science Division | Bayer. Bayer's leadership in Crop Science provides tailored solutions for farmers to plant, grow and protect their harvests using less land, water and energ")
+                elif selected_stock[1]=='IBM':
+                    st.write('International Business Machines Corporation is an American multinational technology corporation headquartered in Armonk, New York, with operations in over 171 countries.')
+                elif selected_stock[1]=='^BKX':
+                    st.write('')
+                
 elif layout=='daily return':
         data_ind['Day_Perc_Change']= data_ind['Close'].pct_change()*100
         data_us['Day_Perc_Change'] = data_us['Close'].pct_change()*100
